a88a39e105530b2d9109aa6c49e4b9b332d63b1d
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2021-07-12 14:59:01 +0300

    Events

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-console.log('Hello World!');
-console.log('Hey, im newbie here!');
-console.log('Hey, im newbie too!');
-console.log('Whats Happening Here guys');
+const EventEmitter = require('events');
+const logger = new EventEmitter();
+const users = []
+const msgs = []
+
+logger.on('message', (msg) => {
+    console.log(`New Message ${msg}`);
+    msgs.push(msg);
+})
+
+logger.on('login', (name) => {
+    console.log(`New User ${name}`);
+    users.push(name);
+})
+
+logger.on('getUsers', () => {
+    console.log('Logged Users:');
+    for (const user of users) {
+        console.log(user + '\n');
+    }
+})
+
+logger.on('getMessages', () => {
+    console.log('Messages:');
+    for (const user of msgs) {
+        console.log(user + '\n');
+    }
+})
+
+
+logger.emit('message', 'Hello World!')
+logger.emit('login', 'Eduard')
+logger.emit('getUsers')
+logger.emit('getMessages')
+
+
