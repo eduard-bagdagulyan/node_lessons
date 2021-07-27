832d4be7b890e2fc58e3348fecdd1a981f33bcd6
Eduard Bagdagulyan <50203643+eduard-bagdagulyan@users.noreply.github.com>
2021-07-27 21:07:38 +0400

    Streams Homework

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ const files = fs.readdirSync(folderPath[0])
 
 for (const file of files) {
     const input = fs.createReadStream(path.join(folderPath[0], file));
-    var output = fs.createWriteStream('folder2.zip');
+    const output = fs.createWriteStream('folder2.zip');
     input.pipe(zip).pipe(output)
 }
