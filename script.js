80520ffd8c4dc3ed15a6dcc4943079693ece3e58
Eduard Bagdagulyan <50203643+eduard-bagdagulyan@users.noreply.github.com>
2021-07-27 21:17:01 +0400

    Streams Homework

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const path = require('path')
 const zip = zlib.createGzip()
 const folderPath = process.argv.slice(2)
 const files = fs.readdirSync(folderPath[0])
+const newFolder = fs.mkdirSync('folder2')
 
 for (const file of files) {
     const input = fs.createReadStream(path.join(folderPath[0], file));
-    const output = fs.createWriteStream('folder2.zip');
+    const output = fs.createWriteStream('folder2/archive.zip');
     input.pipe(zip).pipe(output)
 }
