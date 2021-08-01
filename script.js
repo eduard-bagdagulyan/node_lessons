cf22d64709edfebb75e6af34aeed08a672331f8e
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2021-08-01 22:12:56 +0400

    Express Homework

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
 const express = require('express')
 const app = express()
-let port = 3000
+const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Got a GET request');
+const users = [
+    { id: 1, name: 'James', age: 17 },
+    { id: 2, name: 'Robert', age: 20 },
+    { id: 3, name: 'John', age: 40 },
+    { id: 4, name: 'Jack', age: 14 },
+    { id: 5, name: 'Leo', age: 34 },
+    { id: 6, name: 'Sam', age: 15 }
+]
+
+const numberify = function (req, res, next) {
+    req.params.id = +req.params.id
+    req.params.age = +req.params.age
+    next()
+}
+
+app.get('/users', (req, res) => {
+    res.send(users);
 });
 
-app.post('/', (req, res) => {
-    res.send('Got a POST request');
+app.post('/post/:id/:name/:age', numberify, (req, res) => {
+    users.push(req.params)
+    res.send(users)
 });
 
-app.put('/', (req, res) => {
-    res.send('Got a PUT request');
+app.put('/put/:id/:name/:age', numberify, (req, res) => {
+    let found = false
+    for (const obj of users) {
+        if (obj.id === req.params.id) {
+            found = true
+            obj.name = req.params.name
+            obj.age = req.params.age
+        }
+    }
+    if (found === false) {
+        res.send('Error, id have not found');
+    } else {
+        res.send(users)
+    }
 });
 
-app.delete('/', (req, res) => {
-    res.send('Got a DELETE request');
+app.delete('/del/:id', numberify, (req, res) => {
+    let found = false
+    for (const obj of users) {
+        if (obj.id === req.params.id) {
+            found = true
+        }
+    }
+    if (found === false) {
+        res.send('Error, id have not found');
+    } else {
+        users.splice(req.params.id-1, 1)
+        res.send(users)
+    }
 });
-
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
